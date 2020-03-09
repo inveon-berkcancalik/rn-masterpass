110fcba4fa0399ec91850641ba1d5335a30f9968
Berk Çalık <berkcancalik@gmail.com>
2020-03-09 15:53:51 +0300

    init func

--- android/src/main/java/com/reactlibrary/RnMasterpassModule.java
+++ android/src/main/java/com/reactlibrary/RnMasterpassModule.java
@@ -4,6 +4,10 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.mastercard.mp.checkout.MasterpassError;
+import com.mastercard.mp.checkout.MasterpassInitCallback;
+import com.mastercard.mp.checkout.MasterpassMerchant;
+import com.mastercard.mp.checkout.MasterpassMerchantConfiguration;
 
 public class RnMasterpassModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +24,18 @@ public class RnMasterpassModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void initialize(MasterpassMerchantConfiguration config, final Callback masterpassInitCallback) {
+        MasterpassMerchant.initialize(config, new MasterpassInitCallback() {
+            @Override
+            public void onInitSuccess() {
+                masterpassInitCallback.invoke();
+            }
+
+            @Override
+            public void onInitError(MasterpassError masterpassError) {
+                masterpassInitCallback.invoke(masterpassError);
+            }
+        });
+        //callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
 }
