d561be21721d676d95cdc77921778a3635b48d02
Berk Çalık <berkcancalik@gmail.com>
2020-03-10 10:39:07 +0300

    sdk downgrade, init, checkMasterpass, getCards

--- android/src/main/java/com/reactlibrary/RnMasterpassModule.java
+++ android/src/main/java/com/reactlibrary/RnMasterpassModule.java
@@ -5,20 +5,24 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
-import com.mastercard.mp.checkout.MasterpassError;
-import com.mastercard.mp.checkout.MasterpassInitCallback;
-import com.mastercard.mp.checkout.MasterpassMerchant;
-import com.mastercard.mp.checkout.MasterpassMerchantConfiguration;
-
-import java.util.Locale;
+import cardtek.masterpass.MasterPassServices;
+import cardtek.masterpass.interfaces.CheckMasterPassListener;
+import cardtek.masterpass.interfaces.GetCardsListener;
+import cardtek.masterpass.response.InternalError;
+import cardtek.masterpass.response.ServiceError;
+import cardtek.masterpass.results.CheckMasterPassResult;
+import cardtek.masterpass.results.GetCardsResult;
+import cardtek.masterpass.util.MasterPassInfo;
 
 public class RnMasterpassModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private final MasterPassServices service;
 
     public RnMasterpassModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        this.service = new MasterPassServices(reactContext, "");
     }
 
     @Override
@@ -27,28 +31,53 @@ public class RnMasterpassModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void initialize(ReadableMap _config, final Callback masterpassInitCallback) {
+    public void initialize(ReadableMap config) {
+        service.setMsisdn(config.getString("phone"));
+        MasterPassInfo.setClientID(config.getString("clientId"));
+        MasterPassInfo.setLanguage(config.getString("language"));
+        MasterPassInfo.setMacroMerchantId(config.getString("macroMerchantId"));
+    }
 
-        MasterpassMerchantConfiguration config = new MasterpassMerchantConfiguration.Builder()
-                .setContext(getReactApplicationContext()) //context
-                .setEnvironment(_config.getString("environment")) //environment
-                .setLocale(new Locale("tr","TR")) //locale
-                .setMerchantName(_config.getString("merchantName"))
-                .setExpressCheckoutEnabled(_config.getBoolean("expressCheckoutEnabled"))//if merchant is express enabled
-                .setCheckoutId(_config.getString("checkoutId"))
-                .build();
-
-        MasterpassMerchant.initialize(config, new MasterpassInitCallback() {
+    @ReactMethod
+    public void checkMasterpass(String token, String referenceNo, final Callback callback)
+    {
+        MasterPassServices s = new MasterPassServices(getReactApplicationContext(), "msisdn");
+        s.checkMasterPass(token, referenceNo, new CheckMasterPassListener() {
             @Override
-            public void onInitSuccess() {
-                masterpassInitCallback.invoke(true);
+            public void onSuccess(CheckMasterPassResult checkMasterPassResult) {
+                callback.invoke(checkMasterPassResult);
             }
 
             @Override
-            public void onInitError(MasterpassError masterpassError) {
-                masterpassInitCallback.invoke(masterpassError);
+            public void onServiceError(ServiceError serviceError) {
+                callback.invoke(serviceError);
+            }
+
+            @Override
+            public void onInternalError(InternalError internalError) {
+                callback.invoke(internalError);
             }
         });
-        //callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+
+    @ReactMethod
+    public void getCards(String token, String referanceNo, final Callback callback)
+    {
+        service.getCards(token, referanceNo, new GetCardsListener() {
+            @Override
+            public void onSuccess(GetCardsResult getCardsResult) {
+                callback.invoke(getCardsResult);
+            }
+
+            @Override
+            public void onServiceError(ServiceError serviceError) {
+                callback.invoke(serviceError);
+            }
+
+            @Override
+            public void onInternalError(InternalError internalError) {
+                callback.invoke(internalError);
+            }
+        });
     }
 }
