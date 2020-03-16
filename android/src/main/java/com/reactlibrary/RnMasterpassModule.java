c872142902ab6c006873224d28efd7fb7cd86db4
Berk Çalık <berkcancalik@gmail.com>
2020-03-16 13:39:25 +0300

    masterpass functions

--- android/src/main/java/com/reactlibrary/RnMasterpassModule.java
+++ android/src/main/java/com/reactlibrary/RnMasterpassModule.java
@@ -1,6 +1,7 @@
 package com.reactlibrary;
 
 import android.util.Log;
+import android.widget.TextView;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -19,12 +20,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import cardtek.masterpass.MasterPassServices;
+import cardtek.masterpass.attributes.MasterPassEditText;
 import cardtek.masterpass.interfaces.CheckMasterPassListener;
 import cardtek.masterpass.interfaces.GetCardsListener;
+import cardtek.masterpass.interfaces.PurchaseListener;
+import cardtek.masterpass.interfaces.RegisterCardListener;
 import cardtek.masterpass.response.InternalError;
 import cardtek.masterpass.response.ServiceError;
+import cardtek.masterpass.response.ServiceResult;
 import cardtek.masterpass.results.CheckMasterPassResult;
 import cardtek.masterpass.results.GetCardsResult;
+import cardtek.masterpass.results.PurchaseResult;
+import cardtek.masterpass.results.RegisterCardResult;
 import cardtek.masterpass.util.MasterPassInfo;
 
 public class RnMasterpassModule extends ReactContextBaseJavaModule {
@@ -45,31 +52,30 @@ public class RnMasterpassModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void initialize(ReadableMap config) {
-        Log.d("MASTERPASS_initialize", new Gson().toJson(config));
         service = new MasterPassServices(getReactApplicationContext(), config.getString("phone"));
 
-        if (config.getBoolean("sandbox")) {
-            MasterPassInfo.setUrl("https://test.masterpassturkiye.com/MasterpassJsonServerHandler/v2/");
+        if (config.getString("env").equals("uatui")){
+            MasterPassInfo.setUrl("https://uatui.masterpassturkiye.com/v2");
+        } else if(config.getString("env").equals("test")) {
+            MasterPassInfo.setUrl("https://test.masterpassturkiye.com/MasterpassJsonServerHandler/v2");
         }
 
         MasterPassInfo.setClientID(config.getString("clientId"));
         MasterPassInfo.setLanguage(config.getString("language"));
-
         MasterPassInfo.setMacroMerchantId(config.getString("macroMerchantId"));
     }
 
     @ReactMethod
     public void checkMasterpass(String token, String referenceNo, final Callback callback)
     {
-        Log.d("MP_checkMasterpass1", token);
-        Log.d("MP_checkMasterpass2", referenceNo);
-
         service.checkMasterPass(token, referenceNo, new CheckMasterPassListener() {
             @Override
             public void onSuccess(CheckMasterPassResult checkMasterPassResult) {
                 try {
                     JSONObject obj = new JSONObject(new Gson().toJson(checkMasterPassResult));
                     callback.invoke(JsonConvert.jsonToReact(obj));
+
+
                 } catch (JSONException e) {
                     callback.invoke("#1 JSONObject parsing error.");
                 }
@@ -132,4 +138,102 @@ public class RnMasterpassModule extends ReactContextBaseJavaModule {
             }
         });
     }
+
+    @ReactMethod
+    public void purchase(String token, String referanceNo, String cardName, final Callback callback)
+    {
+        service.purchase(token, cardName, 1, "TEST", referanceNo, new PurchaseListener() {
+            @Override
+            public void onSuccess(PurchaseResult purchaseResult) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(purchaseResult));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#7 JSONObject parsing error.");
+                }
+            }
+
+            @Override
+            public void onVerifyUser(ServiceResult serviceResult) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(serviceResult));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#8 JSONObject parsing error.");
+                }
+            }
+
+            @Override
+            public void onServiceError(ServiceError serviceError) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(serviceError));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#9 JSONObject parsing error.");
+                }
+            }
+
+            @Override
+            public void onInternalError(InternalError internalError) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(internalError));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#10 JSONObject parsing error.");
+                }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void register(String token, String cardNumber, int expireMonth, int expireYear,
+                         String cardName, String referenceNo, final Callback callback)
+    {
+        MasterPassEditText editText = new MasterPassEditText(getReactApplicationContext());
+        editText.setText(cardNumber); //CRASH
+
+
+        service.registerCard(token, editText, expireMonth, expireYear, cardName, referenceNo, null, new RegisterCardListener() {
+            @Override
+            public void onSuccess(RegisterCardResult registerCardResult) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(registerCardResult));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#11 JSONObject parsing error.");
+                }
+            }
+
+            @Override
+            public void onVerifyUser(ServiceResult serviceResult) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(serviceResult));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#12 JSONObject parsing error.");
+                }
+            }
+
+            @Override
+            public void onServiceError(ServiceError serviceError) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(serviceError));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#13 JSONObject parsing error.");
+                }
+            }
+
+            @Override
+            public void onInternalError(InternalError internalError) {
+                try {
+                    JSONObject obj = new JSONObject(new Gson().toJson(internalError));
+                    callback.invoke(JsonConvert.jsonToReact(obj));
+                } catch (JSONException e) {
+                    callback.invoke("#14 JSONObject parsing error.");
+                }
+            }
+        });
+    }
+
 }
